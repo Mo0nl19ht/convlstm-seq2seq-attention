b4ce74f449b110b84e1ad037ff8da4142d8e7ace
Mo0nl19ht <mool2480@gmail.com>
2022-01-18 11:36:43 +0900

    feat: Encoder 기능 추가

--- Encoder.py
+++ Encoder.py
@@ -7,6 +7,7 @@ class Encoder(tf.keras.Model):
         self.enc_num_layers = enc_num_layers
         self.encoder_input_convlstm = ConvLSTMCell(
             hidden_dim=hidden,
+            att_hidden_dim=hidden,
             kernel_size=(3, 3),
             bias=True
         )
@@ -14,51 +15,53 @@ class Encoder(tf.keras.Model):
             self.hidden_encoder_layers = [
                 ConvLSTMCell(
                     hidden_dim=hidden,
+                    att_hidden_dim=hidden,
                     kernel_size=(3, 3),
                     bias=True,
                 ) for _ in range(self.enc_num_layers)
             ]
 
     def call(self, enc_input):
-        h_t, c_t = self.init_hidden(enc_input, 'seq')
+        h_t, c_t, m_t = self.init_hidden(enc_input, 'seq')
         if self.enc_num_layers is not None:
             hidden_h_t = []
             hidden_c_t = []
+            hidden_m_t = []
             for i in range(self.enc_num_layers):
                 hidden_h_t += [self.init_hidden(h_t, i)[0]]
                 hidden_c_t += [self.init_hidden(h_t, i)[1]]
-
+                hidden_m_t += [self.init_hidden(h_t, i)[2]]
         seq_len = enc_input.shape[1]
         for t in range(seq_len):
-            h_t, c_t = self.encoder_input_convlstm(
+            h_t, c_t, m_t = self.encoder_input_convlstm(
                 input_tensor=enc_input[:, t, :, :, :],
-                cur_state=[h_t, c_t]
+                cur_state=[h_t, c_t, m_t]
             )
             input_tensor = h_t
             if self.enc_num_layers is not None:
                 for i in range(self.enc_num_layers):
-                    hidden_h_t[i], hidden_c_t[i] = self.hidden_encoder_layers[i](
+                    hidden_h_t[i], hidden_c_t[i], hidden_m_t[i] = self.hidden_encoder_layers[i](
                         input_tensor=input_tensor,
-                        cur_state=[hidden_h_t[i], hidden_c_t[i]]
+                        cur_state=[hidden_h_t[i], hidden_c_t[i], hidden_m_t[i]]
                     )
                     input_tensor = hidden_h_t[i]
 
         if self.enc_num_layers is not None:
-            return hidden_h_t[-1], hidden_c_t[-1]
+            return hidden_h_t[-1], hidden_c_t[-1], hidden_m_t[-1]
         else:
-            return h_t, c_t
+            return h_t, c_t,m_t
 
     def init_hidden(self, input_tensor, seq):
         if seq == 'seq':
             b, seq_len, h, w, _ = input_tensor.shape
-            h_t, c_t = self.encoder_input_convlstm.init_hidden(
+            h_t, c_t, m_t = self.encoder_input_convlstm.init_hidden(
                 batch_size=b,
                 image_size=(h, w)
             )
         else:
             b, h, w, _ = input_tensor.shape
-            h_t, c_t = self.hidden_encoder_layers[seq].init_hidden(
+            h_t, c_t, m_t = self.hidden_encoder_layers[seq].init_hidden(
                 batch_size=b,
                 image_size=(h, w)
             )
-        return h_t, c_t
+        return h_t, c_t, m_t
